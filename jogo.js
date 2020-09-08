139f581959b3d57f2e746af68edf896f4d007603
Felipe Cardoso <felipedclc@gmail.com>
2020-09-08 18:06:45 -0300

    Ajuste posicao do mosquito dentro da janela

--- jogo.js
+++ jogo.js
@@ -10,4 +10,23 @@ function ajusteTamanhoJanela() {
 
 ajusteTamanhoJanela()
 
+function posicaoRandomica(){
+    var posicaoX = Math.floor(Math.random() * width) - 90;
+    var posicaoY = Math.floor(Math.random() * height) - 90;
 
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    console.log(posicaoX, posicaoY);
+
+    /****CRIANDO O ELEMENTO HTML****/
+    var mosquito = document.createElement('img');
+    mosquito.src = 'imagens/mosca.png'; // atribuindo a variavel mosquito a imagem
+    mosquito.className = 'mosquito1';
+    mosquito.style.left = posicaoX + 'px';
+    mosquito.style.top = posicaoY + 'px';
+    mosquito.style.position = 'absolute';
+    // " . " acessa os atributos html
+
+    document.body.appendChild(mosquito);  // ADD UM FILHO PARA O BODY
+}
