2af5913a718ebe3fbcea337fac96dc1a2a7d961e
Felipe Cardoso <felipedclc@gmail.com>
2020-09-08 17:21:14 -0300

    Redimensionando a tela com onresize

---
+++ jogo.js
@@ -0,0 +1,13 @@
+
+var height = 0;
+var width = 0;
+
+function ajusteTamanhoJanela() {
+    height = window.innerHeight;
+    width = window.innerWidth;
+    console.log(height, width);
+}
+
+ajusteTamanhoJanela()
+
+
